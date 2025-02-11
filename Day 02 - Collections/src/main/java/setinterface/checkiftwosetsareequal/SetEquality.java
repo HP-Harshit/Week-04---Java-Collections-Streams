04cfaba5add2126ab44010ad57a15a20be99342f
HP-Harshit <harshitpatel274@gmail.com>
2025-02-12 00:49:01 +0530

    HP-Harshit Added Solution for Set Interface: Check if Two Sets Are Equal

--- Day 02 - Collections/src/main/java/setinterface/checkiftwosetsareequal/SetEquality.java
+++ Day 02 - Collections/src/main/java/setinterface/checkiftwosetsareequal/SetEquality.java
@@ -1,4 +1,4 @@
-package setinterface;
+package setinterface.checkiftwosetsareequal;
 
 import java.util.HashSet;
 import java.util.List;
--- Day 02 - Collections/src/test/java/setinterface/checkiftwosetsareequal/SetEqualityTest.java
+++ Day 02 - Collections/src/test/java/setinterface/checkiftwosetsareequal/SetEqualityTest.java
@@ -1,4 +1,4 @@
-package setinterface;
+package setinterface.checkiftwosetsareequal;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
